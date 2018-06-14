55881e22f4dfd88c3fb03f654be1c037a71e2709
Tan Huy H. Nguyen <tanhuy@me.com>
2018-06-14 20:02:11 +0900

    documentation page ready

--- main.js
+++ main.js
@@ -1,11 +1,66 @@
 // Animate Smooth Scroll
-$("#view-work").on("click", function() {
-  const images = $("#images").position().top;
+$("#view-document").on("click", function() {
+  const images = $("#main-doc").position().top;
 
   $("html, body").animate(
-        {
-            scrollTop: images
-        },
-            900
-        );
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
+
+$("#view-A").on("click", function() {
+  const images = $("#A").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
+
+$("#view-B").on("click", function() {
+  const images = $("#B").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
+
+$("#view-C").on("click", function() {
+  const images = $("#C").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
+
+$("#view-D").on("click", function() {
+  const images = $("#D").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
+
+$("#view-E").on("click", function() {
+  const images = $("#E").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
 });
